51622000dd06343d1591ee06288e6f144cccff06
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-10-30 00:22:01 +0200

    Add MongoDB Settings Add Dependency Injections in Program

--- CourseWork/Program.cs
+++ CourseWork/Program.cs
@@ -1,29 +1,64 @@
+using Сoursework.Models;
+using Сoursework.Repositories;
+using Сoursework.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+// 1. Налаштування MongoDB
+var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings");
+var connectionString = mongoDbSettings["ConnectionString"] ?? throw new InvalidOperationException("MongoDB ConnectionString not found.");
+var dbName = mongoDbSettings["DatabaseName"] ?? throw new InvalidOperationException("MongoDB DatabaseName not found.");
+
+// Реєстрація MongoDBRepository як Singleton
+builder.Services.AddSingleton(new MongoDBRepository(connectionString, dbName));
+
+// 2. Реєстрація репозиторіїв
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<VisitRepository>();
+builder.Services.AddScoped<PaymentRepository>();
+
+// 3. Реєстрація сервісів (Бізнес-логіка)
+builder.Services.AddScoped<UserService>();
+// Створення нових сервісів
+builder.Services.AddScoped<VisitService>(); 
+builder.Services.AddScoped<PaymentService>(); 
+
+// Додавання сервісів для Razor Pages/MVC та авторизації
+builder.Services.AddRazorPages();
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", options =>
+    {
+        options.Cookie.Name = "DentalClinicAuthCookie";
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/AccessDenied";
+    });
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAdmin", policy => policy.RequireRole("Administrator"));
+    options.AddPolicy("RequireOperator", policy => policy.RequireRole("Administrator", "Operator"));
+    options.AddPolicy("RequireSpecialist", policy => policy.RequireRole("Administrator", "Specialist"));
+    options.AddPolicy("RequirePatient", policy => policy.RequireRole("Patient"));
+});
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Налаштування HTTP-запитів
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+// Включення автентифікації та авторизації
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapRazorPages();
 
 app.Run();
