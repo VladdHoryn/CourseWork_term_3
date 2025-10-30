7315d615d6152f1111d4b98f9210940e036f28ed
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-10-30 22:40:55 +0200

    Add TokenService

---
+++ CourseWork/Controllers/AuthController/TokenService.cs
@@ -0,0 +1,37 @@
+﻿namespace CourseWork.Controllers.AuthController;
+
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Сoursework.Models;
+
+public class TokenService
+{
+    private readonly string _secretKey;
+
+    public TokenService(IConfiguration config)
+    {
+        _secretKey = config["Jwt:Key"] ?? throw new Exception("JWT key not found in config");
+    }
+
+    public string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.Name, user.UserName),
+            new Claim(ClaimTypes.Role, user.UserRole.ToString())
+        };
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var token = new JwtSecurityToken(
+            claims: claims,
+            expires: DateTime.UtcNow.AddHours(2),
+            signingCredentials: creds
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
