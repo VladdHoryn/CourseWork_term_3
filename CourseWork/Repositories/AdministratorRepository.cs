855c2ed200c3b9fc545467ab639608969738d2e9
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-01 12:59:52 +0200

    Add AdministratorRepository

---
+++ CourseWork/Repositories/AdministratorRepository.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using Сoursework.Models;
+using Сoursework.Repositories;
+
+namespace CourseWork.Repositories;
+
+public class AdministratorRepository
+{
+    private readonly IMongoCollection<User> _users;
+
+    public AdministratorRepository(MongoDBRepository db)
+    {
+        _users = db.GetCollection<User>("users");
+    }
+
+    public List<User> GetAllAdmins() =>
+        _users.Find(u => u.UserRole == Role.Administrator).ToList();
+
+    public void DeleteUserById(string id)
+    {
+        _users.DeleteOne(u => u.Id == id);
+    }
+}
