166a4abbc8051e878e83637857296224a25bca85
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-01 12:59:15 +0200

    Add OperatorRepository

---
+++ CourseWork/Repositories/OperatorRepository.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using Сoursework.Models;
+using Сoursework.Repositories;
+
+namespace CourseWork.Repositories;
+
+public class OperatorRepository
+{
+    private readonly IMongoCollection<User> _users;
+
+    public OperatorRepository(MongoDBRepository db)
+    {
+        _users = db.GetCollection<User>("users");
+    }
+
+    public List<User> GetAllOperators() =>
+        _users.Find(u => u.UserRole == Role.Operator).ToList();
+}
