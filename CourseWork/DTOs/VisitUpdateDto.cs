a8485da8bd1084869141842969932b39f61a2dc4
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-06 22:03:49 +0200

    Add VisitUpdateDto

---
+++ CourseWork/DTOs/VisitUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace CourseWork.DTOs;
+
+public class VisitUpdateDto
+{
+    public string Anamnesis { get; set; }
+    public string Diagnosis { get; set; }
+    public string Treatment { get; set; }
+    public string? Recommendations { get; set; }
+    public decimal ServiceCost { get; set; }
+    public decimal MedicationCost { get; set; }
+    public string Status { get; set; }
+}
