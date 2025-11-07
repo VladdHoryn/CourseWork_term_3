a6bf5118fff74c03e77c02b4e65455e4e060aaad
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:43:38 +0200

    Add PaymentDto

---
+++ CourseWork/DTOs/PaymentDto.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseWork.DTOs;
+
+public class PaymentDto
+{
+    public string Id { get; set; }
+    public string VisitId { get; set; }
+    public int PatientMedicalRecord { get; set; }
+    public decimal TotalAmount { get; set; }
+    public decimal PaidAmount { get; set; }
+    public decimal RemainingAmount { get; set; }
+    public string Status { get; set; }
+    public DateTime IssuedDate { get; set; }
+    public DateTime DueDate { get; set; }
+}
