e6a6e72784f430f39ebf1bccf5b63081aac54058
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:46:14 +0200

    Add UserMapper

---
+++ CourseWork/Mappers/UserMapper.cs
@@ -0,0 +1,15 @@
+﻿using CourseWork.DTOs;
+using Сoursework.Models;
+
+namespace CourseWork.Mappers;
+
+public static class UserMapper
+{
+    public static UserDto ToDto(User u) => new()
+    {
+        Id = u.Id,
+        UserName = u.UserName,
+        Role = u.UserRole.ToString(),
+        CreatedAt = u.CreatedAt
+    };
+}
