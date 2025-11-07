c65de2fbe061619b4ec4b739c5f691bfaa98ca4a
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:49:37 +0200

    Add SpecialistMapper

---
+++ CourseWork/Mappers/SpecialistMapper.cs
@@ -0,0 +1,18 @@
+﻿using CourseWork.DTOs;
+using Сoursework.Models;
+
+namespace CourseWork.Mappers;
+
+public static class SpecialistMapper
+{
+    public static SpecialistDto ToDto(User u) => new()
+    {
+        Id = u.Id,
+        UserName = u.UserName,
+        Role = u.UserRole.ToString(),
+        CreatedAt = u.CreatedAt,
+        FullName = u.FullName,
+        Speciality = u.Speciality,
+        DateOfBirth = u.DateOfBirth
+    };
+}
