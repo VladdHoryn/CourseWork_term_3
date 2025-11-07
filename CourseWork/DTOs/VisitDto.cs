f3bf87d52e67f658163ddb4d34e32f31d188e28f
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:43:08 +0200

    Add VisitDto

---
+++ CourseWork/DTOs/VisitDto.cs
@@ -0,0 +1,15 @@
+﻿namespace CourseWork.DTOs;
+
+public class VisitDto
+{
+    public string Id { get; set; }
+    public int PatientMedicalRecord { get; set; }
+    public string SpecialistFullName { get; set; }
+    public DateTime VisitDate { get; set; }
+    public bool IsFirstVisit { get; set; }
+    public string Diagnosis { get; set; }
+    public decimal ServiceCost { get; set; }
+    public decimal MedicationCost { get; set; }
+    public decimal TotalCost { get; set; }
+    public string Status { get; set; }
+}
