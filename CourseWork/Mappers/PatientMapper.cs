2c040c44fb089cefb5da101359841bab619d1610
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:48:57 +0200

    Add PatientMapper

---
+++ CourseWork/Mappers/PatientMapper.cs
@@ -0,0 +1,20 @@
+﻿using CourseWork.DTOs;
+using Сoursework.Models;
+
+namespace CourseWork.Mappers;
+
+public static class PatientMapper
+{
+    public static PatientDto ToDto(User u) => new()
+    {
+        Id = u.Id,
+        UserName = u.UserName,
+        Role = u.UserRole.ToString(),
+        CreatedAt = u.CreatedAt,
+        FullName = u.FullName,
+        Phone = u.Phone,
+        Address = u.Address,
+        DateOfBirth = u.DateOfBirth,
+        MedicalRecordNumber = u.MedicalRecordNumber
+    };
+}
