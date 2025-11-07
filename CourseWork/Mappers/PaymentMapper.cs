1da65e9f554832b4b391bcb363f61e9bcb1cb8f2
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:51:02 +0200

    Add PaymentMapper

---
+++ CourseWork/Mappers/PaymentMapper.cs
@@ -0,0 +1,20 @@
+﻿using CourseWork.DTOs;
+using Сoursework.Models;
+
+namespace CourseWork.Mappers;
+
+public static class PaymentMapper
+{
+    public static PaymentDto ToDto(Payment p) => new()
+    {
+        Id = p.Id,
+        VisitId = p.VisitId,
+        PatientMedicalRecord = p.PatientMedicalRecord,
+        TotalAmount = p.TotalAmount,
+        PaidAmount = p.PaidAmount,
+        RemainingAmount = p.RemainingAmount,
+        Status = p.Status.ToString(),
+        IssuedDate = p.IssuedDate,
+        DueDate = p.DueDate
+    };
+}
