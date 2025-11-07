249a653f13c824c5fba06086e985a09cb7f93cf7
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-07 02:41:26 +0200

    Add SpecialistDto

---
+++ CourseWork/DTOs/SpecialistDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseWork.DTOs;
+
+public class SpecialistDto : UserDto
+{
+    public string? FullName { get; set; }
+    public string? Speciality { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+}
