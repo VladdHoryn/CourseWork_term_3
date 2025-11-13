52fd2c69e5f7ce931d42f12f7f47475a54193dc9
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-13 11:54:02 +0200

    Add GuestController with all logics Add RequestDto

---
+++ CourseWork/DTOs/RegistrationRequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseWork.DTOs;
+
+public class RegistrationRequestDto
+{
+    public string Username { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string? FullName { get; set; }
+    public string? Phone { get; set; }
+    public string? Address { get; set; }
+}
