83bb9940a4ff2045a791361b370321331c9835f1
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-19 14:55:12 +0200

    Fix password Hashing in User

--- CourseWork/Repositories/UserRepository.cs
+++ CourseWork/Repositories/UserRepository.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using Microsoft.AspNetCore.Identity;
+using MongoDB.Driver;
 using Сoursework.Models;
 
 namespace Сoursework.Repositories;
@@ -35,12 +36,27 @@ public class UserRepository
         return Users.Find(u => u.Id == Id).FirstOrDefault();
     }
     
-    public void UpdatePassword(string username, string newPassword)
+    public void UpdatePassword(string username, string newPassword, IPasswordHasher<User> hasher)
     {
-        var filter = Builders<User>.Filter.Eq(u => u.UserName, username);
-        var update = Builders<User>.Update.Set(u => u.PasswordHash, BCrypt.Net.BCrypt.HashPassword(newPassword));
+        if (string.IsNullOrWhiteSpace(newPassword))
+            throw new ArgumentException("New password cannot be empty.", nameof(newPassword));
+
+        // 1. Знайти користувача
+        var user = Users.Find(u => u.UserName == username).FirstOrDefault();
+
+        if (user == null)
+            throw new Exception($"User '{username}' does not exist.");
+
+        // 2. Використати метод User.SetPasswordHash(...)
+        user.SetPasswordHash(newPassword, hasher);
+
+        // 3. Оновити запис в MongoDB
+        var filter = Builders<User>.Filter.Eq(u => u.Id, user.Id);
+        var update = Builders<User>.Update.Set(u => u.PasswordHash, user.PasswordHash);
+
         Users.UpdateOne(filter, update);
     }
+
     
     public void UpdateUser(User updatedUser)
     {
