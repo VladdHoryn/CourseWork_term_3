40ae04dba8221f06d1c4f66b60a3928430e6e6e7
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-26 00:39:18 +0200

    Add AuditLog

---
+++ CourseWork/Models/AuditLog.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CourseWork.Models;
+
+public class AuditLog
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; }
+
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+
+    public string UserId { get; set; }
+    public string UserName { get; set; }
+
+    public string Action { get; set; }    
+    public string Details { get; set; }
+}
