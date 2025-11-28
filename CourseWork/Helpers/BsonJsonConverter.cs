27da1fb2cfd3840cc4e6eb0e07e345ddd55af422
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-28 13:22:47 +0200

    Add Back-end part for SQL quires

---
+++ CourseWork/Helpers/BsonJsonConverter.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+
+namespace CourseWork.Helpers;
+
+public static class BsonJsonConverter
+{
+    public static object ToPlainObject(BsonValue value)
+    {
+        if (value == null || value.IsBsonNull) return null;
+
+        switch (value.BsonType)
+        {
+            case BsonType.ObjectId:
+                return value.AsObjectId.ToString();
+
+            case BsonType.String:
+                return value.AsString;
+
+            case BsonType.Int32:
+                return value.AsInt32;
+
+            case BsonType.Int64:
+                return value.AsInt64;
+
+            case BsonType.Double:
+                return value.AsDouble;
+
+            case BsonType.Boolean:
+                return value.AsBoolean;
+
+            case BsonType.DateTime:
+                return value.ToUniversalTime(); // конвертуємо у DateTime
+
+            case BsonType.Document:
+                var doc = value.AsBsonDocument;
+                return doc.Elements.ToDictionary(e => e.Name, e => ToPlainObject(e.Value));
+
+            case BsonType.Array:
+                var arr = value.AsBsonArray;
+                return arr.Select(ToPlainObject).ToList();
+
+            default:
+                return value.ToString();
+        }
+    }
+
+    public static List<object> ConvertList(IEnumerable<BsonDocument> docs)
+    {
+        return docs.Select(d => ToPlainObject(d)).ToList();
+    }
+}
