6670afdce8707e567d3e77cfe70216c426bff183
VladdHoryn <horyn.vladyslav@chnu.edu.ua>
2025-11-28 14:04:46 +0200

    Final

--- CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
+++ CourseWork/obj/Debug/net9.0/CourseWork.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57b69a05895bd42a3b6f57f604360cceb34a1712")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7930002ef877a0a2ce95017303bbc466a9d35807")]
 [assembly: System.Reflection.AssemblyProductAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CourseWork")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
